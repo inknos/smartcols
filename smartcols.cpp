df13d76590ec7e53b8604a7102d5448267608f98
Nicola Sella <nsella@redhat.com>
2020-06-26 17:35:16 +0200

    Same behavior reproduced with function

--- smartcols.cpp
+++ smartcols.cpp
@@ -56,6 +56,80 @@ MapToTable::print()
     scols_print_table(this->tb);
 }
 
+void print_table_as_together(listpkgmap listPkgMap) {
+
+    struct libscols_table * tb;
+    enum { COL_KEY, COL_VALUE };
+
+    setlocale(LC_ALL, "");
+
+    tb = scols_new_table();
+
+    scols_table_new_column(tb, "KEY",   2, SCOLS_FL_WRAP);
+    scols_table_new_column(tb, "VALUE", 2, SCOLS_FL_WRAP);
+
+    scols_table_enable_noheadings(tb, 1);
+    scols_table_set_column_separator(tb, " : ");
+
+    for ( auto it = listPkgMap.begin(); it != listPkgMap.end(); it++) {
+
+        auto pkg = *it;
+        auto ln = scols_table_new_line(tb, NULL);
+        auto lv = scols_table_new_line(tb, NULL);
+
+        scols_line_set_data(ln, COL_KEY, "name");
+        scols_line_set_data(ln, COL_VALUE, std::get<std::string>(pkg["name"]).c_str());
+
+        scols_line_set_data(lv, COL_KEY, "version");
+        scols_line_set_data(lv, COL_VALUE, std::get<std::string>(pkg["version"]).c_str());
+
+
+        if ( std::next(it) != listPkgMap.end() ) {
+            auto le = scols_table_new_line(tb, NULL);
+            scols_line_set_data(le, COL_KEY, "");
+            scols_line_set_data(le, COL_VALUE, "");
+        }
+    }
+        scols_print_table(tb);
+        scols_unref_table(tb);
+
+}
+
+void print_table_as_separate(listpkgmap listPkgMap) {
+
+
+    for ( auto it = listPkgMap.begin(); it != listPkgMap.end(); it++) {
+        struct libscols_table * tb;
+        enum { COL_KEY, COL_VALUE };
+
+        setlocale(LC_ALL, "");
+
+        tb = scols_new_table();
+
+        scols_table_new_column(tb, "KEY",   2, SCOLS_FL_WRAP);
+        scols_table_new_column(tb, "VALUE", 2, SCOLS_FL_WRAP);
+
+        scols_table_enable_noheadings(tb, 1);
+        scols_table_set_column_separator(tb, " : ");
+
+        auto pkg = *it;
+        auto ln = scols_table_new_line(tb, NULL);
+        auto lv = scols_table_new_line(tb, NULL);
+
+        scols_line_set_data(ln, COL_KEY, "name");
+        scols_line_set_data(ln, COL_VALUE, std::get<std::string>(pkg["name"]).c_str());
+
+        scols_line_set_data(lv, COL_KEY, "version");
+        scols_line_set_data(lv, COL_VALUE, std::get<std::string>(pkg["version"]).c_str());
+
+        scols_print_table(tb);
+        scols_unref_table(tb);
+
+        if ( std::next(it) != listPkgMap.end() ) { std::cout << std::endl; }
+    }
+
+}
+
 int main() {
     listpkgmap listPkgMap = { // vector
         { // map
@@ -68,6 +142,7 @@ int main() {
         }
     };
 
+    std::cout << "=================" << std::endl;
     for ( auto it = listPkgMap.begin(); it != listPkgMap.end(); it++ ) {
         MapToTable mtt;
         auto pkg = *it;
@@ -77,5 +152,10 @@ int main() {
         if ( std::next(it) != listPkgMap.end() ) { std::cout << std::endl; }
     }
 
+    std::cout << "=================" << std::endl;
+    print_table_as_separate(listPkgMap);
+    std::cout << "=================" << std::endl;
+    print_table_as_together(listPkgMap);
+    std::cout << "=================" << std::endl;
 }
 
